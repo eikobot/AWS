e9bb68b502fa1f1977b3032ad08e2bf6c15c5a8c
kazaamjt <kazaamjt@gmail.com>
2023-10-14 15:03:23 +0200

    Add EC2KeyPair and basis for EC2Instance

--- aws/__init__.py
+++ aws/__init__.py
@@ -5,7 +5,7 @@ such IAM credentials, IAM roles, VPCs, VMs and EKS clusters.
 from eikobot.core.handlers import CRUDHandler, HandlerContext
 from eikobot.core.helpers import EikoBaseModel
 
-from aws import aws_api
+from . import api
 
 
 class IAMRoleModel(EikoBaseModel):
@@ -15,6 +15,9 @@ class IAMRoleModel(EikoBaseModel):
 
     __eiko_resource__ = "IAMRole"
 
+    name: str
+    permissions: list[str]
+
 
 class IAMRoleHandler(CRUDHandler):
     """
@@ -23,8 +26,82 @@ class IAMRoleHandler(CRUDHandler):
 
     __eiko_resource__ = "IAMRole"
 
-    async def read(self, ctx: HandlerContext) -> None:
+    async def read(self, ctx: HandlerContext[IAMRoleModel]) -> None:
         pass
 
-    async def create(self, ctx: HandlerContext) -> None:
+    async def create(self, ctx: HandlerContext[IAMRoleModel]) -> None:
         pass
+
+
+class EC2KeyPairModel(EikoBaseModel):
+    """
+    Represents a keypair used for ssh in EC2 instances.
+    While officially these are region bound,
+    this model can be used to deploy to different regions as needed.
+    """
+
+    __eiko_resource__ = "EC2KeyPair"
+
+    name: str
+    public_key: str
+
+    # We only check once per region
+    _regions: list[str] = []
+
+    def enforce(self, region: str, ctx: HandlerContext, dry_run: bool = False) -> None:
+        """
+        Makes sure the key pair exists in the given region,
+        and updates the public key or tags if needed.
+        """
+        if region in self._regions:
+            return
+
+        keys = api.get_key_pairs(region)
+        prev_key = keys.get(self.name)
+        if prev_key is None:
+            ctx.debug("Creating EC2KeyPair.")
+            api.import_key_pair(
+                self.name,
+                self.public_key.encode("utf-8"),
+                region,
+                dry_run,
+                tags={"eikobot-managed": "yes"},
+            )
+
+        # split to remove OpenSSH annotations
+        elif prev_key.public_key.split()[1] != self.public_key.split()[1]:
+            ctx.debug("Updating EC2KeyPair.")
+            api.delete_key_pair(region, prev_key.key_pair_id)
+            api.import_key_pair(
+                self.name,
+                self.public_key.encode("utf-8"),
+                region,
+                dry_run,
+                tags={"eikobot-managed": "yes"},
+            )
+
+        self._regions.append(region)
+
+
+class EC2InstanceModel(EikoBaseModel):
+    """
+    Standard representation of an EC2 instance.
+    """
+
+    __eiko_resource__ = "EC2Instance"
+
+    name: str
+    region: str
+    key_pair: EC2KeyPairModel
+
+
+class EC2InstanceHandler(CRUDHandler):
+    """
+    Deploys an EC2 instance and the key pair if required.
+    """
+
+    __eiko_resource__ = "EC2Instance"
+
+    async def create(self, ctx: HandlerContext[EC2InstanceModel]) -> None:
+        ctx.resource.key_pair.enforce(ctx.resource.region, ctx)
+        ctx.deployed = True
---
+++ aws/api.py
@@ -0,0 +1,99 @@
+"""
+Abstracted boto3 functionality to make it easier to use.
+"""
+import datetime
+from typing import TYPE_CHECKING
+
+import boto3
+from pydantic import BaseModel
+
+if TYPE_CHECKING:
+    from mypy_boto3_ec2 import EC2Client
+    from mypy_boto3_ec2.type_defs import TagTypeDef
+
+
+class BotoCache:
+    """
+    Allows for easy creation and automatic caching of Boto resources.
+    """
+
+    _ec2_clients: dict[str, "EC2Client"] = {}
+
+    @classmethod
+    def get_ec2_client(cls, region: str) -> "EC2Client":
+        """
+        Retrieves a cached client or creates and caches it.
+        """
+        client = cls._ec2_clients.get(region)
+        if client is None:
+            client = boto3.client("ec2", region_name=region)
+            cls._ec2_clients[region] = client
+
+        return client
+
+
+def import_key_pair(
+    key_name: str,
+    public_key: bytes,
+    region: str,
+    dry_run: bool = False,
+    tags: dict[str, str] | None = None,
+) -> None:
+    """
+    Import a key pair to a specific region.
+    """
+    _tags: list["TagTypeDef"] = []
+    if tags is not None:
+        for key, value in tags.items():
+            _tags.append({"Key": key, "Value": value})
+
+    client = BotoCache.get_ec2_client(region)
+    client.import_key_pair(
+        DryRun=dry_run,
+        KeyName=key_name,
+        PublicKeyMaterial=public_key,
+        TagSpecifications=[
+            {
+                "ResourceType": "key-pair",
+                "Tags": _tags,
+            }
+        ],
+    )
+
+
+class EC2KeyPair(BaseModel):
+    """
+    Result of querying the AWS api for key pairs
+    """
+
+    key_pair_id: str
+    fingerprint: str
+    name: str
+    type: str
+    public_key: str
+    create_time: datetime.datetime
+
+
+def get_key_pairs(region: str) -> dict[str, EC2KeyPair]:
+    """
+    Returns all ssh keys in a given region.
+    """
+    client = BotoCache.get_ec2_client(region)
+    raw_key_pairs = client.describe_key_pairs(IncludePublicKey=True)["KeyPairs"]
+    keys: dict[str, EC2KeyPair] = {}
+    for raw_pair in raw_key_pairs:
+        keys[raw_pair["KeyName"]] = EC2KeyPair(
+            key_pair_id=raw_pair["KeyPairId"],
+            fingerprint=raw_pair["KeyFingerprint"],
+            name=raw_pair["KeyName"],
+            type=raw_pair["KeyType"],
+            public_key=raw_pair["PublicKey"],
+            create_time=raw_pair["CreateTime"],
+        )
+
+    return keys
+
+
+def delete_key_pair(region: str, key_pair_id: str) -> None:
+    client = BotoCache.get_ec2_client(region)
+    client.delete_key_pair(KeyPairId=key_pair_id)
